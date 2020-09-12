24f023b15b836b0234f1da04f516932b5e23c961
Wajid Areeb <s.w.a.11wajid@gmail.com>
2020-09-12 12:24:02 +0530

    Search on url Added not in form
    Search only works when types in url box not in form

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index1, name="index1"),
+    path("wiki/", views.index, name="index"),
+    path("wiki/<str:q>", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
+import markdown2
 
 
+# If the User vist direct website not "wiki/"
+def index1(request):
+    return HttpResponseRedirect(reverse("index"))
+
+
+# This is the Main Page of the website
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+
+# This is the search result page of the website
+def search(request, q):
+    if q and q != '':
+        content = util.get_entry(q)
+        if content:
+            return render(request, "encyclopedia/search.html", {
+                "content": markdown2.markdown(content)
+            })
+        else:
+            return render(request, "encyclopedia/404.html")
+    else:
+        return HttpResponseRedirect(reverse("index"))
