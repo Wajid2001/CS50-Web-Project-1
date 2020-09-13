bd63b9db663441f66bcbcbace92ce1634720fb67
Wajid Areeb <67428865+Wajid2001@users.noreply.github.com>
2020-09-13 20:55:16 +0530

    Random Page created
    Add the functionality of random page on website

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index1, name="index1"),
     path("wiki/", views.index, name="index"),
-    path("wiki/<str:q>", views.search, name="search")
+    path("wiki/<str:q>", views.search, name="search"),
+    path("wiki/random/", views.randomPage, name="randomPage")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,9 +4,11 @@ from django.urls import reverse
 
 from . import util
 import markdown2
-
+import random
 
 # If the User vist direct website not "wiki/"
+
+
 def index1(request):
     return HttpResponseRedirect(reverse("index"))
 
@@ -24,9 +26,16 @@ def search(request, q):
         content = util.get_entry(q)
         if content:
             return render(request, "encyclopedia/search.html", {
+                "title": q,
                 "content": markdown2.markdown(content)
             })
         else:
             return render(request, "encyclopedia/404.html")
     else:
         return HttpResponseRedirect(reverse("index"))
+
+
+# This is render to a random page
+def randomPage(request):
+    r = random.choice(util.list_entries())
+    return search(request, r)
